299b834df3fb9bc7da2f4b9a92be2437611378eb
Mohanad <mohannadnasar99@gmail.com>
2023-11-07 21:35:44 +0200

    feat: changeAmountOfItem (BE)

--- backend/Controllers/PatientController.js
+++ backend/Controllers/PatientController.js
@@ -3,8 +3,8 @@ const Medicine = require('../Models/Medicine');
 const Cart =  require('../Models/Cart');
 
 exports.registerPatient = async (req, res) => {
-  const newPatient = await Patient.create(req.body);
 
+  const newPatient = await Patient.create(req.body);
   res.status(201).json({
     message: 'patient created successfully',
     data: newPatient,
@@ -14,9 +14,9 @@ exports.registerPatient = async (req, res) => {
 exports.addOverTheCounterMedicine = async(req , res) => {
 
   try {
-    const { medicineId, quantity } = req.body;
     const { patientId } = req.params;
-
+    const { medicineId, quantity } = req.body;
+    
     const medicine = await Medicine.findById(medicineId);
     if (!medicine) {
       return res.status(404).json({ error: 'Medicine not found' });
@@ -37,7 +37,8 @@ exports.addOverTheCounterMedicine = async(req , res) => {
 
     await cart.save();
     res.status(201).json(cart);
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(400).json({ error: err.message });
   }
    
@@ -45,36 +46,57 @@ exports.addOverTheCounterMedicine = async(req , res) => {
 };
 
 exports.viewCart = async (req, res) => {
+
   try {
     const { patientId } = req.params;
     const cart = await Cart.findOne({ patientId }).populate('items.medicineId');
     res.json(cart);
-  } catch (err) {
+  } 
+  catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
 exports.removeItemFromCart = async(req,res) => {
+
   try {
     const { patientId } = req.params;
     const { medicineId } = req.body;
     const cart = await Cart.findOne({ patientId });
+
     const itemIndex = cart.items.findIndex((item) => item.medicineId.toString() === medicineId);
     if (itemIndex !== -1) {
       cart.items.splice(itemIndex, 1);
+      await cart.save();
+      res.json(cart);
     }
-    await cart.save();
-    res.json(cart);
-
+    else{
+      return res.status(404).json({ error: 'Item not found' });
+    }
   }
   catch (err) {
     res.status(500).json({ error: err.message });
   }  
-
 };
 
 exports.updateQuantityOfItem = async(req,res) => {
 
-
-
+  try {
+    const { patientId } = req.params;
+    const { medicineId , quantity } = req.body;
+    const cart = await Cart.findOne({ patientId });
+    
+    const itemIndex = cart.items.findIndex((item) => item.medicineId.toString() === medicineId);
+    if (itemIndex !== -1) {
+      cart.items[itemIndex].quantity = quantity;
+      await cart.save();
+      res.json(cart);
+    }
+    else{
+     res.status(404).json({ error: 'Item not found' });
+    } 
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
