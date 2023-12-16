c7fa07805424d8a5fd5526c26c400d140b9f3e01
mohamed khaled <mohamed.106@icloud.com>
2023-12-16 21:23:48 +0200

    pharmacist frontend (not final)

--- frontend/src/components/AddMedicine.jsx
+++ frontend/src/components/AddMedicine.jsx
@@ -1,8 +1,33 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import TopNavigationPharmacist from "./TopNavigationPharmacist";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import "../Css/AddMedicine.css";
+
+
+
+
 
 function AddMedicine() {
+  const [prescription, setPrescription] = React.useState('no'); // default value
+
+  const handlePrescriptionChange = (event) => {
+    setPrescription(event.target.value);
+  };
   const [res, setRes] = useState(null);
   const [formData, setFormData] = useState(
     {
@@ -13,6 +38,7 @@ function AddMedicine() {
       quantity: "",
       medicalUse: "",
       ingredients: [""],
+      prescription: false,
     },
     [],
     { withCredentials: true }
@@ -45,101 +71,194 @@ function AddMedicine() {
       ingredients: formData.ingredients.filter(
         (ingredient) => ingredient.trim() !== ""
       ),
+      prescription: prescription === 'yes', // Convert to boolean
     };
 
     const response = await axios.post(
       "http://localhost:8000/api/v1/pharmacist/addMedicine",
       medicine,
-      { withCredentials: true }
+      { withCredentials: true,
+        // headers: {
+        //   'Content-Type': 'multipart/form-data'
+        // }
+      }
     );
     setRes(response);
   };
 
   return (
     <div>
-      <h2>Add Medicine</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Medicine Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={(e) => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>Picture:</label>
-          <input
-            type="text"
-            name="picture"
-            value={formData.picture}
-            onChange={(e) => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>Price:</label>
-          <input
-            type="number"
-            name="price"
-            value={formData.price}
-            onChange={(e) => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>Quantity:</label>
-          <input
-            type="number"
-            name="quantity"
-            value={formData.quantity}
-            onChange={(e) => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>medical use:</label>
-          <input
-            type="text"
-            name="medicalUse"
-            value={formData.medicalUse}
-            onChange={(e) => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>Description:</label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={(e) => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>Ingredients:</label>
-          {formData.ingredients.map((ingredient, index) => (
-            <div key={index}>
-              <input
-                type="text"
-                name="ingredients"
-                value={ingredient}
-                onChange={(e) => handleChange(e, index)}
-                required
-              />
-            </div>
-          ))}
-          <button type="button" onClick={addIngredient}>
-            Add Ingredient
-          </button>
-        </div>
-        <button type="submit">Add Medicine</button>
-      </form>
-      {res && <div>new medicine added</div>}
+            <TopNavigationPharmacist link="/pharmacists/home" />
+
+      <div className="add-medicine-container"> 
+
+      {/* Include TopNavigationPharmacist or other components if needed */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: 2,
+        }}
+      >
+        <AddCircleOutlinedIcon sx={{ fontSize: 50, marginBottom: 2, color: 'primary.main' }} />
+        <Typography variant="h2" sx={{ fontSize: 50, marginBottom: 2, color: 'primary.main' }}>
+          Add Medicine
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Card variant="outlined" sx={{ width: '100%', maxWidth: 800 }}>
+            <CardContent>
+              <Grid container spacing={2}>
+                {/* First Column */}
+                <Grid item xs={12} md={6}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Medicine Name"
+                      variant="outlined"
+                      name="name"
+                      value={formData.name}
+                      onChange={(e) => handleChange(e)}
+                      required
+                      fullWidth
+                      sx={{ marginBottom: 2 }}
+                    />
+                    <TextField
+                      id="outlined-basic"
+                      label=""
+                      variant="outlined"
+                      name="picture"
+                      value={formData.picture}
+                      onChange={(e) => handleChange(e)}
+                      required
+                      fullWidth
+                      sx={{ marginBottom: 2 }}
+                      type="file"
+                    />
+                    <TextField
+                      id="outlined-basic"
+                      label="Price"
+                      variant="outlined"
+                      name="price"
+                      type="number"
+                      value={formData.price}
+                      onChange={(e) => handleChange(e)}
+                      required
+                      fullWidth
+                      sx={{ marginBottom: 2 }}
+                    />
+                    <TextField
+                      id="outlined-basic"
+                      label="Quantity"
+                      variant="outlined"
+                      name="quantity"
+                      type="number"
+                      value={formData.quantity}
+                      onChange={(e) => handleChange(e)}
+                      required
+                      fullWidth
+                      sx={{ marginBottom: 2 }}
+                    />
+                  </Box>
+                </Grid>
+                {/* Second Column */}
+                <Grid item xs={12} md={6}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <TextField
+                      id="outlined-basic"
+                      label="Medical Use"
+                      variant="outlined"
+                      name="medicalUse"
+                      value={formData.medicalUse}
+                      onChange={(e) => handleChange(e)}
+                      required
+                      fullWidth
+                      sx={{ marginBottom: 2 }}
+                    />
+                    <TextField
+                      id="outlined-basic"
+                      label="Description"
+                      variant="outlined"
+                      name="description"
+                      multiline
+                      value={formData.description}
+                      onChange={(e) => handleChange(e)}
+                      required
+                      fullWidth
+                      rows={4}
+                      sx={{ marginBottom: 2 }}
+                    />
+                    <Grid container spacing={2} alignItems="center">
+                      <Grid item xs={6}>
+                        <TextField
+                          id="outlined-basic"
+                          label="Ingredients"
+                          variant="outlined"
+                          name="ingredients"
+                          value={formData.ingredients[0]}
+                          onChange={(e) => handleChange(e, 0)}
+                          required
+                          fullWidth
+                        />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Button type="button" onClick={addIngredient} variant="contained" sx={{ width: '100%' }}>
+                          Add Ingredient
+                        </Button>
+                      </Grid>
+                    </Grid>
+                    {formData.ingredients.slice(1).map((ingredient, index) => (
+                      <TextField
+                        key={index + 1}
+                        id={`outlined-basic-${index + 1}`}
+                        label={`Ingredient ${index + 1}`}
+                        variant="outlined"
+                        name="ingredients"
+                        value={ingredient}
+                        onChange={(e) => handleChange(e, index + 1)}
+                        required
+                        fullWidth
+                        sx={{ marginBottom: 2 }}
+                      />
+                    ))}
+                  </Box>
+                </Grid>
+              </Grid>
+                    <FormControl component="fieldset" sx={{ marginTop: 2 }}>
+        <FormLabel component="legend">Prescription</FormLabel>
+        <RadioGroup
+          row
+          aria-label="prescription"
+          name="prescription"
+          value={prescription}
+          onChange={handlePrescriptionChange}
+        >
+          <FormControlLabel value="no" control={<Radio />} label="No" />
+          <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        </RadioGroup>
+      </FormControl>
+              <Button type="submit" variant="contained" color="primary" sx={{ marginTop: 2, width: '100%' }}>
+                Add Medicine
+              </Button>
+            </CardContent>
+          </Card>
+        </form>
+        {res && <div>new medicine added</div>}
+      </Box>
+      </div>
     </div>
   );
+
 }
 
 export default AddMedicine;
